d7a395c7544edd0c9efb1a2e266c335faad0e449
ahmad-sukran-mamun <64555816+ahmad-sukran-mamun@users.noreply.github.com>
2020-05-14 17:00:13 +0700

    Create jobshet 1 C.c
    Buat program menampilkan NIM masing – masing mahasiswa.

---
+++ jobshet 1 C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main (){
+	printf("AHMAD SUKRAN MA;MUN-F1B019016/KELOMPOK5 \n");
+
+	int nim[6]= {14,3,15,49,37,44};
+	
+	printf ("Nim ke-1 = %i\n",nim[0]);
+	printf ("Nim ke-2 = %i\n",nim[1]);
+	printf ("Nim ke-3 = %i\n",nim[2]);
+	printf ("Nim ke-4 = %i\n",nim[3]);
+	printf ("Nim ke-5 = %i\n",nim[4]);
+	printf ("Nim ke-6 = %i\n",nim[5]);
+	printf ("Nim ke-7 = %i\n",nim[6]);
+	
+}
