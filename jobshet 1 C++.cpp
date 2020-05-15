fbedb12c561243d5e91e39b4c74aac015e303083
ahmad-sukran-mamun <64555816+ahmad-sukran-mamun@users.noreply.github.com>
2020-05-15 12:11:01 +0700

    Create jobshet 1 C++.cpp
    Buat program menampilkan NIM masing – masing mahasiswa.

---
+++ jobshet 1 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"AHMAD SUKRAN MA'MUN'-F1B019O16/kelompok 8\n\n";
+int nim[6]= {4,13,27,8,33};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+}
+
